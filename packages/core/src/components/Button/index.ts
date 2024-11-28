e28b5688b21ff19410cf398d0d0e51acc9c155dc
sagarmusabbir <sagarmusabbir@gmail.com>
2024-11-28 07:20:26 +0600

    fix: add explicit type exports for Button component

--- packages/core/src/components/Button/index.ts
+++ packages/core/src/components/Button/index.ts
@@ -1,1 +1,3 @@
-export { Button, ButtonProps, buttonVariants } from "./Button";
+// export { Button, ButtonProps, buttonVariants } from "./Button";
+export { Button, buttonVariants } from "./Button";
+export type { ButtonProps } from "./Button";
